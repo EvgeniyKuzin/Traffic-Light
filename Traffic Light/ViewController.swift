2a4e649dda0b5173ebb9ab3416927d2a3fc8de36
EvgeniyKuzin <vaglover@yandex.ru>
2023-01-30 22:35:43 +0300

    UI and logic completed

--- Traffic Light/ViewController.swift
+++ Traffic Light/ViewController.swift
@@ -8,12 +8,43 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var redLight: UIView!
+    @IBOutlet var yellowLight: UIView!
+    @IBOutlet var greenLight: UIView!
+    
+    @IBOutlet var startButton: UIButton!
+    
+    let lightOff: CGFloat = 0.3
+    let lightOn: CGFloat = 1
+    
+    private var roundRadius :CGFloat {
+        redLight.frame.height / 2
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        startButton.layer.cornerRadius = 10
+        
+        redLight.layer.cornerRadius = roundRadius
+        yellowLight.layer.cornerRadius = roundRadius
+        greenLight.layer.cornerRadius = roundRadius
     }
 
-
+    @IBAction func buttonTapped() {
+        startButton.setTitle("NEXT", for: .normal)
+        
+        if redLight.alpha.isLess(than: lightOn) && yellowLight.alpha.isLess(than: lightOn) {
+            redLight.alpha = lightOn
+            greenLight.alpha = lightOff
+        } else if yellowLight.alpha.isLess(than: lightOn) {
+            yellowLight.alpha = lightOn
+            redLight.alpha = lightOff
+        } else {
+            greenLight.alpha = lightOn
+            yellowLight.alpha = lightOff
+        }
+        
+        
+    }
 }
 
